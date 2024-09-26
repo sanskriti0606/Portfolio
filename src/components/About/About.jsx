40e4880391807c6022a51dbb398e184293a3bf39
sanskriti0606 <104161059+sanskriti0606@users.noreply.github.com>
2024-09-27 04:03:05 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -19,8 +19,8 @@ export const About = () => {
             <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
               <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
+                Front-end expertise includes React.js from basic to advanced levels, along with essential CSS frameworks like Bootstrap and Tailwind CSS. I'm a frontend developer with experience in building responsive
+                and optimized sites.
               </p>
             </div>
           </li>
@@ -29,18 +29,18 @@ export const About = () => {
             <div className={styles.aboutItemText}>
               <h3>Backend Developer</h3>
               <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
+                Back-end expertise includes Node.js and Express.js, enabling the development of robust and scalable server-side applications. I have experience developing fast and optimised back-end systems
+                and APIs.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
+              <h3>Database</h3>
               <p>
-                I have designed multiple landing pages and have created design
-                systems as well
+                Database
+              Proficient in database management with experience in MongoDB databases. Skilled in MySQL for relational database design, and in MongoDB for handling unstructured data and building scalable applications.
               </p>
             </div>
           </li>
